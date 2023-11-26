a093d9b45d315ddfa553f0aa2758b69b0c01aafd
Qusai GAZAWY <candotube@gmail.com>
2023-11-27 02:18:19 +0300

    Right before forms

--- lecture3/settings.py
+++ lecture3/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'hello',
+    'newyear',
+    'lecture3',
+    'tasks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- lecture3/urls.py
+++ lecture3/urls.py
@@ -17,7 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
+    path('', views.index, name="index"),
     path('admin/', admin.site.urls),
-    path('hello/', include('hello.urls'))
+    path('hello/', include('hello.urls')),
+    path('newyear/', include('newyear.urls')),
+    path('tasks/', include('tasks.urls'))
 ]
+
+
